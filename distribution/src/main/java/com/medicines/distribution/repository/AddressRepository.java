a389181c2f4b7b629f2e15bf5689b627b8e33215
luka-rakin <116868040+luka-rakin@users.noreply.github.com>
2023-11-16 17:35:35 +0100

    feat/Company administrator - started

--- distribution/src/main/java/com/medicines/distribution/repository/AddressRepository.java
+++ distribution/src/main/java/com/medicines/distribution/repository/AddressRepository.java
@@ -1,8 +1,23 @@
 package com.medicines.distribution.repository;
 
 import com.medicines.distribution.model.Address;
+import com.medicines.distribution.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<Address,Integer> {
 
+    default Address update(Integer id, Address updatedAddress){
+        Address address = findById(id).orElse(null);
+
+        if(address != null){
+            address.setCountry(updatedAddress.getCountry());
+            address.setCity(updatedAddress.getCity());
+            address.setStreet(updatedAddress.getStreet());
+            address.setNumber(updatedAddress.getNumber());
+            address.setZipCode(updatedAddress.getZipCode());
+            save(address);
+        }
+
+        return address;
+    }
 }
