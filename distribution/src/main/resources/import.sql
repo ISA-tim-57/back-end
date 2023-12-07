e3aeaf7b9f0907717df3ea9edf134194f2edc4aa
luka-rakin <116868040+luka-rakin@users.noreply.github.com>
2023-12-07 14:24:19 +0100

    Insert script fix

--- distribution/src/main/resources/import.sql
+++ distribution/src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT into address(id, city, country, "number", street, zip_code) VALUES (1, 'Backa Palanka', 'Srbija', '27', 'Blok Partizan', '21400');
-INSERT into address(id, city, country, "number", street, zip_code) VALUES (2, 'Novi Sad', 'Srbija', '33', 'Novosadskog sajma', '21000');
-INSERT into address(id, city, country, "number", street, zip_code) VALUES (3, 'Novi Sad', 'Srbija', '44', 'Strazilovska', '21000');
+INSERT into address( city, country, "number", street, zip_code) VALUES ('Backa Palanka', 'Srbija', '27', 'Blok Partizan', '21400');
+INSERT into address( city, country, "number", street, zip_code) VALUES ('Novi Sad', 'Srbija', '33', 'Novosadskog sajma', '21000');
+INSERT into address( city, country, "number", street, zip_code) VALUES ('Novi Sad', 'Srbija', '44', 'Strazilovska', '21000');
 
-INSERT into company(address_id, id, rating, description, name) VALUES (1, 1, 5, 'Opis 1', 'Kompa1');
-INSERT into company(address_id, id, rating, description, name) VALUES (2, 2, 4, 'Opis 2', 'Kompa2');
-INSERT into company(address_id, id, rating, description, name) VALUES (3, 3, 5, 'Opis 3', 'Ubisoft');
+INSERT into company(address_id, rating, description, name) VALUES ( 1, 5, 'Opis 1', 'Kompa1');
+INSERT into company(address_id, rating, description, name) VALUES ( 2, 4, 'Opis 2', 'Kompa2');
+INSERT into company(address_id, rating, description, name) VALUES ( 3, 5, 'Opis 3', 'Ubisoft');
 
-INSERT INTO user(address_id, id, company_info, email, name, password, phone, profession, surname, username)VALUES (1, 1, 'adsa', 'user@gmail.com', 'Pera', '12345', '225-883', 'apotekar', 'Peric', 'pere');
+INSERT INTO user(address_id, company_info, email, name, password, phone, profession, surname, username)VALUES (1, 'adsa', 'user@gmail.com', 'Pera', '12345', '225-883', 'apotekar', 'Peric', 'pere');
