0899401606d6a2a33a8bf31596bffb320668c78c
luka-rakin <116868040+luka-rakin@users.noreply.github.com>
2024-01-23 11:14:25 +0100

    import script comment

--- distribution/src/main/resources/import.sql
+++ distribution/src/main/resources/import.sql
@@ -11,7 +11,7 @@ INSERT into company(address_id, rating, description, name, working_hours_begin,w
 INSERT into company(address_id, rating, description, name, working_hours_begin,working_hours_end) VALUES ( 2, 4, 'Opis 2', 'Kompa2','08:00:00','16:00:00');
 INSERT into company(address_id, rating, description, name, working_hours_begin,working_hours_end) VALUES ( 3, 5, 'Opis 3', 'Ubisoft','09:00:00','17:00:00');
 
-
+//za sve korisnike password je "password123"
 INSERT INTO app_user(address_id, company_info, email, name, password, phone, profession, surname, username)VALUES (1, 'adsa', 'user@gmail.com', 'Pera', '$2a$10$eO5z6Ul6K5vQdtZYRj1YtuAMe.TOoZCKuuFHasGuSMV9/EmnSQ1qO', '225-883', 'apotekar', 'Peric', 'pera');
 INSERT INTO app_user(address_id, company_info, email, name, password, phone, profession, surname, username,company_id)VALUES (2, 'adsa', 'mika@gmail.com', 'Mika', '$2a$10$eO5z6Ul6K5vQdtZYRj1YtuAMe.TOoZCKuuFHasGuSMV9/EmnSQ1qO', '225-883', 'apotekar', 'Mikic', 'mika',1);
 INSERT INTO app_user(address_id, company_info, email, name, password, phone, profession, surname, username,company_id)VALUES (3, 'adsa', 'nikola@gmail.com', 'Nikola', '$2a$10$eO5z6Ul6K5vQdtZYRj1YtuAMe.TOoZCKuuFHasGuSMV9/EmnSQ1qO', '225-883', 'apotekar', 'Nikolic', 'nidza',1);
